d6c91ffb2009e3441259763e4d69e3a9d7fb7395
feuvpi <fredvpg@gmail.com>
2023-09-08 18:18:09 -0300

    configuring navigational properties manually

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -27,6 +27,21 @@ namespace banking_dotnet_api.Models
         public DbSet<User> Users { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
 
+
+        // -- Manually configure the relationship of navigation properties
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.Sender)        // -- A Transaction has one Sender (User)
+                .WithMany(u => u.SentTransactions) // -- A User has many SentTransactions
+                .HasForeignKey(t => t.SenderId);  // -- Foreign key in Transaction
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.Receiver)      // -- A Transaction has one Receiver (User)
+                .WithMany(u => u.ReceivedTransactions) // -- A User has many ReceivedTransactions
+                .HasForeignKey(t => t.ReceiverId);    // -- Foreign key in Transaction
+        }
+
     }
 }
 
