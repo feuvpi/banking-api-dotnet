903f56dbb54666abdc3011283247be6eb2da47f3
feuvpi <fredvpg@gmail.com>
2023-09-09 16:48:29 -0300

    adding method to set hashed password using sha256 encryption

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,14 @@
 ﻿using banking_dotnet_api.DTO;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace banking_dotnet_api.Models
 {
-    public class User {  
-    
+    public class User {
+
+        private string passwordSalt; // -- Store the salt
+
         public User(UserDTO user)
         {
             FirstName = user.FirstName;
@@ -36,13 +40,32 @@ namespace banking_dotnet_api.Models
         public string? Password { get; set; }
 
         public decimal Balance { get; set; }
-
+        private static RandomNumberGenerator rng = RandomNumberGenerator.Create();
 
         // -- Navigation property for transactions where this user is the sender
         public ICollection<Transaction>? SentTransactions { get; set; }
 
         // -- Navigation property for transactions where this user is the receiver
         public ICollection<Transaction>? ReceivedTransactions { get; set; }
+
+        public void SetPassword(string password)
+        {
+            using(var sha256 = SHA256.Create())
+            {
+                byte[] saltBytes = new byte[16]; // -- generate a random salt
+                using(var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(saltBytes);
+                }
+
+                this.passwordSalt = Convert.ToBase64String(saltBytes);
+
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password + this.passwordSalt);
+                byte[] hashedByutes = sha256.ComputeHash(passwordBytes);
+                this.Password = Convert.ToBase64String(hashedByutes);
+
+            }
+        }
 
     }
 }
