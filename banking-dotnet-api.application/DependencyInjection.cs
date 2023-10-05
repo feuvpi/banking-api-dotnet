4b03656de6869cbe876a3ec3ec877e0e56c7034e
feuvpi <fredvpg@gmail.com>
2023-10-05 11:00:11 -0300

    feat: Configure Dependency Injection for Application Layer

---
+++ banking-dotnet-api.application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+namespace banking_dotnet_api.application
+{
+    public static class DependencyInjection
+    {
+
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+            var assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+    }
+    }
+}
