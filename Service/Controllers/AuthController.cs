45990ad12b721bb3c52187c8d1c57670aaf38664
feuvpi <fredvpg@gmail.com>
2023-10-18 20:31:01 -0300

    project refactoring

---
+++ Service/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("auth/open")]
+        public ActionResult GetSemAutenticacao()
+        {
+            return Ok("Rota sem autenticação");        
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("auth/protected")]
+        public ActionResult ProtectedRoute()
+        {
+            return Ok("Metodo com autenticação");
+        }
+
+    }
+}
