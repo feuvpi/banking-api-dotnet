2247865295f125033d5bc0c68de32d7947ff8ba5
feuvpi <fredvpg@gmail.com>
2023-12-10 23:08:37 -0300

    .

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,3 +1,9 @@
+using Infrastructure.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using BaseContext = Infrastructure.Context.BaseContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<BaseContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+// -- use jwt bearer authentication
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://securetoken.google.com/350481044738";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "https://securetoken.google.com/350481044738",
+            ValidateAudience = true,
+            ValidAudience = "https://securetoken.google.com/350481044738",
+            ValidateLifetime = true
+        };
+    });
+
+// -- fonte: https://balta.io/blog/aspnetcore-3-1-autenticando-sua-api-com-google-via-firebase
+
+
 
 var app = builder.Build();
 
